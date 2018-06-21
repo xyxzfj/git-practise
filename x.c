9342fdb24927afba3781408d7e0a0f5797f3b5a6
Franky Zhang <xyxzfj@gmail.com>
2018-06-21 23:06:10 +0800

    Create x.c

---
+++ x.c
@@ -0,0 +1,7 @@
+Step 4 — Disable Password Authentication on your Server
+If you were able to log into your account using SSH without a password, you have successfully configured SSH-key-based authentication to your account. However, your password-based authentication mechanism is still active, meaning that your server is still exposed to brute-force attacks.
+
+Before completing the steps in this section, make sure that you either have SSH-key-based authentication configured for the root account on this server, or preferably, that you have SSH-key-based authentication configured for a non-root account on this server with sudo privileges. This step will lock down password-based logins, so ensuring that you will still be able to get administrative access is crucial.
+
+Once you've confirmed that your remote account has administrative privileges, log into your remote server with SSH keys, either as root or with an account with sudo privileges. Then, open up the SSH daemon's configuration file:
+
